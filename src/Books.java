7ef52429b263e1f7590ba8f00b7d7a583ac9e8b9
bavula028 <135271061+bavula028@users.noreply.github.com>
2023-07-10 18:02:22 -0500

    updated

--- src/Books.java
+++ src/Books.java
@@ -2,16 +2,36 @@ public class Books {
 
     private String ISBN;
     private String title;
-    private String author;
-    private String cost;
+    private String author1;
+    private String author2;
+    private String author3;
+    private String author4;
+    private String author5;
+    private String author6;
+    private String author7;
+    private String author8;
+    private String author9;
+    private String author10;
+    private double cost;
     private String[] bookDetails;
 
-    public Books(String isbnInput, String titleInput, String authorInput, String costInput){
+
+
+    public Books(String isbnInput, String titleInput, double costInput, String author1Input, String author2Input, String author3Input, String author4Input, String author5Input, String author6Input, String author7Input, String author8Input, String author9Input, String author10Input){
 
         ISBN = isbnInput;
         title = titleInput;
-        author = authorInput;
         cost = costInput;
+        author1 = author1Input;
+        author2 = author2Input;
+        author3 = author3Input;
+        author4 = author4Input;
+        author5 = author5Input;
+        author6 = author6Input;
+        author7 = author7Input;
+        author8 = author8Input;
+        author9 = author9Input;
+        author10 = author10Input;
 
     }
 
@@ -19,13 +39,30 @@ public class Books {
         bookDetails = commaSeparatedLine.split(", ");
         ISBN = bookDetails[0];
         title = bookDetails[1];
-        author = bookDetails[2];
-        cost = bookDetails[3];
+        cost = Double.parseDouble(bookDetails[2]);
+        author1 = bookDetails[3];
+        author2 = bookDetails[4];
+        author3 = bookDetails[5];
+        author4 = bookDetails[6];
+        author5 = bookDetails[7];
+        author6 = bookDetails[8];
+        author7 = bookDetails[9];
+        author8 = bookDetails[10];
+        author9 = bookDetails[11];
+        author10 = bookDetails[12];
+
     }
 
 
     public void printBookDetails() {
-        System.out.println("ISBN Number = " + ISBN + "\nTitle: " + title + "\nAuthor: " + author + "\nCost: " + cost);
+        System.out.println("ISBN Number = " + ISBN + "\nTitle: " + title + "\nCost: " + cost + "\nAuthor(s): " + "\n" + author1 + "\n" + author2 + "\n" + author3 + "\n" + author4 + "\n" + author5 + "\n" + author6 + "\n" + author7 + "\n" + author8 + "\n" + author9 + "\n" + author10);
+
     }
+
+    public double getCost(){
+        System.out.println(cost);
+        return cost;
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,26 @@ public class Main {
 
     public static void main (String[]args){
 
-        Books book1 = new Books ("ISBN0001", "How To Program In Java", "John Doe", "20.00");
+        Books javaBook = new Books ("ISBN0001", "How To Program In Java", 20.00, "Sally Smith", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+
+        Books cPlusPlusBook = new Books ("ISBN0002", "How To Program in C++", 19.99, "John Doe", " ", " ", " ", " ", " ", " ", " ", " ", " ");
+
+        Books javaScriptBook = new Books ("ISBN0001, How to program in JavaScript, 19.21, Adam,  ,  ,  ,  ,  ,  ,  ,  ,  ");
+
+        //CART
+        Cart myCart = new Cart();
+        myCart.addBooks(javaBook);
+        myCart.addBooks(cPlusPlusBook);
+        myCart.addBooks(javaScriptBook);
 
 
-        Books book2 = new Books ("ISBN0002", "How To Program in C++", "Sally Smith", "19.99");
+        //ITERATOR TEST
+        System.out.println("Cart Total: " + myCart.getTotal());
 
-        Books lineFormat = new Books ("ISBN0001, How to program in Java, John Doe, 19.21");
-        lineFormat.printBookDetails();
+
+
+
+
     }
 
     }
